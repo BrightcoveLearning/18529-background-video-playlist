6b681374cc86dea85dee65b4878248984abd26a8
Matthew Boles <mboles@brightcove.com>
2019-12-16 15:54:10 -0500

    WIP

--- plugin/background-playlist.js
+++ plugin/background-playlist.js
@@ -1,12 +1,13 @@
 videojs.registerPlugin('backgroundPlaylist', function() {
-  var myPlayer = this;
+  var myPlayer = this,
+    counter = 0;
 
-// +++ Set the playlist to repeat, but stop after 3 time through +++
+// +++ Set the playlist to repeat, but stop after 2 time through +++
   myPlayer.playlist.repeat(true);
 
    myPlayer.on('playlistitem',function(){
      counter ++;
-     if (counter > 8){
+     if (counter > 4){
        myPlayer.playlist.repeat(false);
      }
    })

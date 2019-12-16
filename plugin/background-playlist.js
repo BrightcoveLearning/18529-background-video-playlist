1c8facc83f19a2d6ff06db462d70dde149f6528d
Matthew Boles <mboles@brightcove.com>
2019-12-16 15:44:36 -0500

    Stop repeat after x number of times

--- plugin/background-playlist.js
+++ plugin/background-playlist.js
@@ -1,8 +1,15 @@
 videojs.registerPlugin('backgroundPlaylist', function() {
   var myPlayer = this;
 
-  // +++ Set the playlist to repeat +++
+// +++ Set the playlist to repeat, but stop after 3 time through +++
   myPlayer.playlist.repeat(true);
+
+   myPlayer.on('playlistitem',function(){
+     counter ++;
+     if (counter > 8){
+       myPlayer.playlist.repeat(false);
+     }
+   })
 
   // +++ Display the title and description +++
   myPlayer.on('loadstart',function(){

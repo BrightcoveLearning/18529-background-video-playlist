2e34924b29a7abdf3333ca728c8eb46867da3536
Matthew Boles <mboles@brightcove.com>
2019-12-17 13:55:32 -0500

    Minor correction

--- plugin/background-playlist.js
+++ plugin/background-playlist.js
@@ -2,7 +2,7 @@ videojs.registerPlugin('backgroundPlaylist', function() {
   var myPlayer = this,
     counter = 0;
 
-// +++ Set the playlist to repeat, but stop after 2 time through +++
+// +++ Set the playlist to repeat, but stop after 2 times through +++
   myPlayer.playlist.repeat(true);
 
    myPlayer.on('playlistitem',function(){
